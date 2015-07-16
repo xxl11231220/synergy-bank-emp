f6d247ebec89621850506547bc8222cca4f035b8
Nagendra <nagendra.yadav.niit@gmail.com>
2015-07-16 15:43:31 -0700

    Leaning Git HUB

---
+++ src/main/java/com/bas/admin/aop/NagendraSuccess.java
@@ -0,0 +1,19 @@
+package com.bas.admin.aop;
+
+/**
+ * 
+ * @author nagendra.yadav
+ *  Please checkout this file
+ *
+ */
+public class NagendraSuccess {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
